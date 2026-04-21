28a01ed449d71c18d83ff07adb3e471687ae481d
timzowen <timzowen@gmail.com>
2026-04-21 12:51:20 +0300

    compose article practice

--- app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
+++ app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
@@ -4,26 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.timzowen.theandroidseries.ui.theme.TheAndroidSeriesTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TheAndroidSeriesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ComposeArticle(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +38,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeArticle(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp
+        )
+
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = stringResource(R.string.article_description_one),
+            textAlign = TextAlign.Justify
+
+        )
+
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(R.string.article_description_two),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheAndroidSeriesTheme {
-        Greeting("Android")
+        ComposeArticle()
     }
 }
