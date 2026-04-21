5ea29837e2ce0fe2b524fdae253ab9e207ef7990
timzowen <timzowen@gmail.com>
2026-04-21 21:23:45 +0300

    removed justify text alignment

--- app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
+++ app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.timzowen.theandroidseries.ui.theme.TheAndroidSeriesTheme
@@ -93,7 +92,6 @@ fun QuadrantComponent(
         )
         Text(
             text = description,
-            textAlign = TextAlign.Justify
         )
     }
 }
