28c9bbeac435a451de7bff42e6fd4c9e02ad55d2
timzowen <timzowen@gmail.com>
2026-05-08 22:06:46 +0300

    affiramtions app

--- app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
+++ app/src/main/java/com/timzowen/theandroidseries/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.timzowen.theandroidseries.affirmations.Affirmation
+import com.timzowen.theandroidseries.data.DataSource
 import com.timzowen.theandroidseries.ui.theme.TheAndroidSeriesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +35,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheAndroidSeriesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    AffirmationApp(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxWidth()
                     )
                 }
             }
@@ -31,17 +47,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun AffirmationApp(modifier: Modifier = Modifier) {
+    AffirmationList(
+        modifier = modifier,
+        affirmations = DataSource().loafAffirmations()
     )
+}
+
+@Composable
+fun AffirmationList(affirmations: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier.padding(20.dp)) {
+        items(affirmations) { affirmation ->
+            AffirmationCard(
+                affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TheAndroidSeriesTheme {
-        Greeting("Android")
-    }
+fun AffirmationCardPreview(modifier: Modifier = Modifier) {
+    AffirmationCard(
+        Affirmation(
+            imageResourceId = R.drawable.image1,
+            stringResourceId = R.string.affirmation1
+        )
+    )
 }
